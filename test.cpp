e60c3c18fa730b7cf8b3546ee7f2ec97ebba917e
Max Khon <fjoe@samodelkin.net>
2022-01-26 23:38:56 +0700

    - Use explicit whole class template instantiation - Use __declspec(dllexport) when building with MS VC

--- test.cpp
+++ test.cpp
@@ -7,4 +7,11 @@ void A<T>::print()
 	std::cout << this->_t << std::endl;
 }
 
-template void A<const char *>::print();
+#ifdef _MSC_VER
+#define LIBTEST_EXPORT __declspec(dllexport)
+#else
+#define LIBTEST_EXPORT
+#endif
+
+template LIBTEST_EXPORT class A<const char *>;
+//template LIBTEST_EXPORT void A<const char *>::print();
